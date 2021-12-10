22e310fecc4e302713b7808c88b7042a8212e50b
Geraldo Moura <geraldo.moura@ymail.com>
2021-12-10 16:24:52 -0600

    created user form parent component

---
+++ src/components/FormUserDetails.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/styles/AppBar';
+import TextField from 'material-ui/styles/TextField';
+import RaisedButton from 'material-ui/styles/RaisedButton';
+
+export class FormUserDetails extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hi from Form User Details</h1>
+            </div>
+        )
+    }
+}
+
+export default FormUserDetails
