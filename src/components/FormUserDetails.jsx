a6a241e3eb9e819613a60472a914f74ade7963b4
Geraldo Moura <geraldo.moura@ymail.com>
2021-12-14 15:27:39 -0600

    fixing compatiblity

--- src/components/FormUserDetails.jsx
+++ src/components/FormUserDetails.jsx
@@ -1,15 +1,35 @@
 import React, { Component } from 'react'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBar from 'material-ui/styles/AppBar';
-import TextField from 'material-ui/styles/TextField';
-import RaisedButton from 'material-ui/styles/RaisedButton';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import TextField from '@material-ui/core/TextField';
 
 export class FormUserDetails extends Component {
+    continue = e => {
+        e.preventDefault();
+        this.props.nextStep();
+    }
+
     render() {
+        const { values } = this.props;
+
         return (
-            <div>
-                <h1>Hi from Form User Details</h1>
-            </div>
+            <>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton edge="start" className="" color="inherit" aria-label="menu">
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" className="">
+                            Enter User Details
+                        </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+            </>
         )
     }
 }
